aaee7cf36ae01bf91900968b7c488be21f4e3fc2
rohitaryan0301 <rg479270@gmail.com>
2025-08-21 23:35:07 +0530

    just commit

---
+++ StarPattern/a.java
@@ -0,0 +1,13 @@
+public class a{
+    public static void main(String [] arg){
+        for(int line=1;line<=5;line++){
+            for(int huch=1;huch<=line;huch++){
+                  System.out.print("# ");
+            }
+             System.out.println();
+        }
+        // System.out.println("#");
+    }
+
+    
+}
