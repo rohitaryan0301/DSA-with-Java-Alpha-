d089862f107bdc2de2c94858967e38279fbea310
rohitaryan0301 <rg479270@gmail.com>
2025-09-06 00:49:01 +0530

    commit

---
+++ pattern2/a.java
@@ -0,0 +1,20 @@
+public class a {
+    public static void show(int row, int col){
+        for(int i=1;i<=row;i++){
+            for(int j=1;j<=col;j++){
+                if(i==1 || i==row || j==1 || j==col){
+                    System.out.print("*");
+                }
+                else {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+    }
+    public static void main(String[] args) {
+        int row=9;
+        int col=6;
+    show(row,col);
+    }
+}
---
+++ pattern2/b.java
@@ -0,0 +1,18 @@
+public class b {
+    public static void showw(int row, int colm){
+        for(int i=1;i<=row;i++){
+            for(int j=1;j<=colm-i;j++){
+                System.out.print(" ");
+            }
+            for(int j=1;j<=i;j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+    public static void main(String[] args) {
+        int row=5;
+        int colm=6;
+        showw(row,colm);
+    }
+}
