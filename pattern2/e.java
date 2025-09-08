82b0117b9578a2b87162e82e37f0ca7bcce286ce
rohitaryan0301 <rg479270@gmail.com>
2025-09-08 23:13:17 +0530

    update

--- pattern2/e.java
+++ pattern2/e.java
@@ -1,3 +1,5 @@
+
+/*  diamond 
 public class e {
     public static void dimd(int n){
         for(int i=1;i<=n;i++){
@@ -25,3 +27,50 @@ public class e {
         dimd(4);
     }
 }
+    
+
+    // pyramid 
+
+    public class e{
+        public static void pmd(int n){
+            for(int i=1;i<n;i++){
+for(int j=1;j<=n-i;j++){
+    System.out.print(" ");
+}
+for(int j=1;j<=i;j++){
+    System.out.print(i+" ");
+}
+System.out.println();
+            }
+        }
+        public static void main(String[] args) {
+            pmd(5);
+        }
+    }
+
+
+    */
+
+    // pallindrome 
+
+    public class e{
+        public static void pdm(int n){
+            
+            for(int i=1;i<=n;i++){
+                 for(int j=1;j<n-i;j++){
+                System.out.print(" ");
+            }
+                for(int j=i;j>=1;j--){
+                    System.out.print(j);
+                }
+                for(int j=2;j<=i;j++){
+                    System.out.print(j);
+                }
+                System.out.println();
+            }
+        }
+
+        public static void main(String[] args) {
+            pdm(5);
+        }
+    }
