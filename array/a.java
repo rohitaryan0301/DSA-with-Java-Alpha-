299aac4b14982c8e805c744b7325099cdc2cae45
rohitaryan0301 <rg479270@gmail.com>
2025-09-09 23:57:41 +0530

    last

---
+++ array/a.java
@@ -0,0 +1,16 @@
+
+import java.util.*;
+
+public class a{
+    public static void main(String[] args) {
+        int a[]=new int[3];
+        Scanner sc= new Scanner(System.in);
+        a[0]=sc.nextInt();
+        a[1]=sc.nextInt();
+        a[2]=sc.nextInt();
+    
+        System.out.println("physics :"+ a[0]);
+        System.out.println("chem : "+ a[1]);
+        System.out.println("math : "+a[2]);
+    }
+}
