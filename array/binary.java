b51cd85f8bdf7286d7d30c1587f3f26c0474aaf4
rohitaryan0301 <rg479270@gmail.com>
2025-09-14 23:48:52 +0530

    c

---
+++ array/binary.java
@@ -0,0 +1,16 @@
+public class binary {
+public static int binaryget(int ab[],int key){
+int low=0;
+int high=ab.length-1;
+int mid=(low+high)/2;
+for(int i=0;i<)
+return i;
+}
+
+    public static void main(String[] args) {
+        int ab[]={12,34,76,78};
+        int key=34;
+        binaryget(ab, key);
+        System.out.println("Item found : "+ ab[i]);
+    }
+}
