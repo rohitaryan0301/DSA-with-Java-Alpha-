776d2ce6c4b8ee22aac815faebb4d94f6e3c5865
rohitaryan0301 <rg479270@gmail.com>
2025-09-15 23:13:25 +0530

    ok

--- array/binary.java
+++ array/binary.java
@@ -1,16 +1,56 @@
+
+
+
+
+/* 
 public class binary {
 public static int binaryget(int ab[],int key){
 int low=0;
 int high=ab.length-1;
-int mid=(low+high)/2;
-for(int i=0;i<)
-return i;
+8while(low<=high){
+    int mid=(low+high)/2;
+    if(ab[mid]==key){
+        return mid;
+    }
+    else if(ab[mid]<key){
+        low=mid+1;
+    }
+    else{
+        high=mid-1;
+    }
+}
+return -1;
 }
 
     public static void main(String[] args) {
         int ab[]={12,34,76,78};
-        int key=34;
-        binaryget(ab, key);
-        System.out.println("Item found : "+ ab[i]);
+        int key=77;
+        
+        System.out.println("Item found : "+ binaryget(ab,key));
     }
+}
+
+*/
+
+// reverse array
+
+public class binary{
+public static void reverse(int num[]){
+    int first=0;
+int last=num.length-1;
+while(first<last){
+    int temp=num[first];
+    num[first]=num[last];
+    num[last]=temp;
+    first++;
+    last--;
+}
+}
+public static void main(String[] args) {
+    int num[]={10,20,45,48,87};
+     reverse(num);
+     for(int i=0;i<num.length;i++){
+        System.out.print(num[i] + " ");
+     }
+}
 }
