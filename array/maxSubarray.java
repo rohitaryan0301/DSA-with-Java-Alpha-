949b5ff7b269e5f88fd66a711542b45746c0a1be
rohitaryan0301 <rg479270@gmail.com>
2025-09-18 23:31:22 +0530

    ok

--- array/maxSubarray.java
+++ array/maxSubarray.java
@@ -2,21 +2,37 @@ public class maxSubarray {
     public static int maxsub(int a[]){
         int currsum=0;
         int maxsum=Integer.MIN_VALUE;
+
+// for reduce time n^2
+int prefix[]= new int[a.length];
+prefix[0]=a[0];
+for(int i=1;i<prefix.length;i++){
+    prefix[i]=prefix[i-1]+a[i];
+}
         for(int i=0;i<a.length;i++){
             int start=i;
             for(int j=i;j<a.length;j++){
                 int end=j;
+
+
+currsum=start==0 ? prefix[end]:prefix[end]-prefix[start-1];
+
+
+
+// Brute force method time complexity=n^3
+               /* 
                 currsum=0;
                 for(int k=start;k<=end;k++){
                     currsum += a[k];
                 }
                 System.out.println(currsum);
+                */
             if(maxsum<currsum){
                 maxsum=currsum;
             }
             }
         }
-            System.out.println("max sum" + maxsum);
+            System.out.println("max sum " + maxsum);
         
         return 1;
     }
